f25a2cb071071aa11f2d53762dd9561174e69c59
Jason Ackerman <jackerman09@gmail.com>
2013-03-08 14:45:44 -0500

    Removed | Home from Title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -12,8 +12,12 @@ describe "Static pages" do
 
     it "should have the right title" do
   		visit '/static_pages/home'
-  		page.should have_selector('title', :text => "#{base_title} | Home")
- 	end
+  		page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App")
+ 	  end
+    it "should not have a custome page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text=> '| Home')
+    end
   end
 
   describe "Help page" do
