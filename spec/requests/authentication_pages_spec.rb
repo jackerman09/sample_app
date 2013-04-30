b201e5681498ce2e9d8b0584bfcecc40509e5aff
Jason Ackerman <jackerman09@gmail.com>
2013-04-30 19:42:20 -0400

    Users must be signed in to edit any user info

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -32,22 +32,48 @@ describe "Authentication" do
 		describe "with valid information" do
 			let(:user) { FactoryGirl.create(:user) }
 			before do
-				valid_signin(user)
+				sign_in user
 				
 				# The following is in spec/support/utilities.rb
-					# fill_in "Email",	with: user.email.upcase
-					# fill_in "Password", with: user.password
-					# click_button "Sign in"
+					# def sign_in(user)
+					# 	visit signin_path
+					# 	fill_in "Email",	with: user.email
+					# 	fill_in "Password", with: user.password
+					# 	click_button "Sign in"
+					# 	# Sign in when not using Capybara as well.
+					# 	cookies[:remember_token] = user.remember_token
+					# end
 				#####
 			end
+			
 			it { should have_selector('title', text: user.name) }
 			it { should have_link('Profile', href: user_path(user)) }
+			it { should have_link('Settings', href: edit_user_path(user)) }
 			it { should have_link('Sign out', href: signout_path) }
 			it { should_not have_link('Sign in', href: signin_path) }
 
 			describe "followed by signout" do
 				before { click_link "Sign out" }
 				it { should have_link('Sign in') }
+			end
+		end
+	end
+
+	describe "authorization" do
+		describe "for non-signed-in users" do
+			let(:user) { FactoryGirl.create(:user) }
+
+			describe "in the Users controller" do
+				
+				describe "visiting the edit page" do
+					before { visit edit_user_path(user) }
+					it { should have_title('Sign in') }
+				end
+
+				describe "submitting to the update action" do
+					before { put user_path(user) }
+					specify { response.should redirect_to(signin_path) }
+				end
 			end
 		end
 	end
