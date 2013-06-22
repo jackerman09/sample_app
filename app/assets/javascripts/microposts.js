82dd6f5110d04b644cc0a011e725ff8fc535ec9f
Jason Ackerman <jackerman09@gmail.com>
2013-06-21 20:31:51 -0400

    Chapter 10 exercises

---
+++ app/assets/javascripts/microposts.js
@@ -0,0 +1,10 @@
+function updateCountdown() {
+	// 140 is the max message length
+	var remaining = 140 - $('#micropost_content').val().length;
+	$('.countdown').text(remaining + ' characters remaining');
+}
+
+$(document).ready(function($){
+	$('#micropost_content').change(updateCountdown);
+	$('#micropost_content').keyup(updateCountdown);
+});
